496150ae29a0b48fe73ea6092b379459b07f70b9
amitxparmar <amitparmar901@gmail.com>
2025-07-23 22:00:04 +0530

    fix(backend): CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,5 @@
 import cors from "cors";
-import express from "express";
+import express, { type ErrorRequestHandler, type NextFunction, type Request, type Response } from "express";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.route";
 import expensesRouter from "./routes/expense.route"
@@ -14,13 +14,23 @@ const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
 app.use(express.json());
 app.use(
   cors({
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
     origin: [CLIENT_URL],
     credentials: true,
   })
 );
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
+app.set("trust proxy", 1);
 connectDB()
+
+app.options("*", cors());
+
+app.use((err: ErrorRequestHandler, req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Credentials", "true");
+  next(err);
+});
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Hello from Bun + TypeScript + Express!");
